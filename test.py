8a2a42f8b5fae6042bfbce8d8ea2a376bd67a450
nnamdi <ago.nnamdi@gmail.com>
2022-03-17 18:12:39 +0000

    mo

---
+++ test.py
@@ -0,0 +1,15 @@
+import socket 
+import platform 
+from datetime import datetime  
+hostname = socket.gethostname()    
+IPAddr = socket.gethostbyname(hostname) 
+
+currentdate = datetime.now()
+timestamp = datetime.timestamp(currentdate)
+
+print("Date and time is:", currentdate)
+print("Timestamp is:", timestamp)   
+print("Your Computer Name is:" + hostname)    
+print("Your Computer IP Address is:" + IPAddr) 
+print(platform.node())
+print(platform.system())
